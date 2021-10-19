2a531552a684424788083fc70b15bccb701c18c6
hanana1253 <hanana1253@gmail.com>
2021-10-19 18:41:49 +0900

    feat: 자신의 기록이 순위권 밖의 경우 새로운 li요소 만들어 붙이는 로직 추가
    순위권 밖의 경우 아래 result div 높이가 줄어들며(class로 처리)  내 기록을 보여주는 li 요소를 추가하도록 작업

--- src/js/rank.js
+++ src/js/rank.js
@@ -3,25 +3,56 @@ const fetchedData = [
   { username: 'Bareum', record: { mm: 0, ss: 0, ms: 10 } },
   { username: 'Chaeyoung', record: { mm: 0, ss: 10, ms: 0 } },
   { username: 'Sohyeong', record: { mm: 0, ss: 20, ms: 0 } },
-  { username: 'Hangyul', record: { mm: 0, ss: 30, ms: 0 } },
-  { username: 'Fastcampus', record: { mm: 0, ss: 40, ms: 0 } }
+  { username: 'Fastcampus', record: { mm: 0, ss: 40, ms: 0 } },
+  { username: 'Fastcampus2', record: { mm: 0, ss: 50, ms: 0 } },
+  { username: 'Fastcampus3', record: { mm: 0, ss: 60, ms: 0 } },
+  { username: 'Fastcampus4', record: { mm: 0, ss: 70, ms: 0 } },
+  { username: 'Fastcampus5', record: { mm: 0, ss: 80, ms: 0 } },
+  { username: 'Fastcampus6', record: { mm: 0, ss: 90, ms: 0 } },
+  { username: 'Fastcampus7', record: { mm: 1, ss: 0, ms: 0 } },
+  { username: 'Hangyul', record: { mm: 1, ss: 30, ms: 0 } },
+  { username: 'Fastcampus8', record: { mm: 2, ss: 40, ms: 0 } },
+  { username: 'Fastcampus9', record: { mm: 3, ss: 40, ms: 0 } }
 ];
 
 const render = () => {
-  // myName은 전역변수 상태를 가져올 예정이라 임시로 선언해둠
-  const myName = 'Hangyul';
+  // 아직 로컬스토리지에 저장하지 않아서 가져올 수가 없으므로 아래 주석처리
+  // const fetchedData = window.localStorage('rankingList');
+
+  // myName, myRank, myRecord는 전역변수 상태를 가져올 예정이라 임시로 선언해둠
+  const myName = 'Chaeyoung';
+  const myRank = 2;
+  const myRecord = { mm: 1, ss: 30, ms: 0 };
 
   document.querySelector('.ranking-list').innerHTML =
-    '<li class="header-row"><span>Rank</span> <span>Username</span><span>Record</span></li>' +
+    '<li class="header-row"><span>Rank</span><span>Username</span><span>Record</span></li>' +
     fetchedData
-      .map((userData, index) => `<li ${userData.username === myName ? 'class="my-record"': ''}><span>${index}</span><span>${userData.username}</span><span>${formatRecord(userData.record)}</span></li>`)
+      .slice(0, 5)
+      .map(
+        (userData, index) =>
+          `<li ${userData.username === myName ? 'class="my-record"' : ''}>
+            <span>${index+1}</span>
+            <span>${userData.username}</span>
+            <span>${formatRecord(userData.record)}</span>
+          </li>`
+      )
       .join('');
 
   // my-rank와 my-result는 전역변수 상태를 가져와 넣을 예정
-  document.querySelector('.my-rank').textContent = '4';
-  document.querySelector('.my-result').textContent = '00:30:00';
+  document.querySelector('.my-rank').textContent = myRank;
+  document.querySelector('.my-result').textContent = formatRecord(myRecord);
 
   document.querySelector('.total-players').textContent = fetchedData.length;
+
+  if (myRank < 5) return;
+  const newListItem = document.createElement('li');
+  newListItem.classList.add('added');
+  newListItem.innerHTML = `<span>${myRank}</span>
+      <span>${myName}</span>
+      <span>${formatRecord(myRecord)}</span>`;
+  document.querySelector('.ranking-list').appendChild(newListItem);
+  document.querySelector('.result').classList.add('added');
+  
 };
 
 // utils로 빼둘 함수
@@ -30,6 +61,5 @@ const formatRecord = (() => {
   const format = n => (n < 10 ? '0' + n : n + '');
   return ({ mm, ss, ms }) => `${format(mm)}:${format(ss)}:${format(ms)}`;
 })();
-
 
 document.addEventListener('DOMContentLoaded', render);
