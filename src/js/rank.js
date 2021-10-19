b84409a48e0d9cddb16e5a4c35ba75fa4e8cf060
hanana1253 <hanana1253@gmail.com>
2021-10-19 16:56:56 +0900

    feat: Create rank.js and put mock data
    TODO: Append elemnts based on the mock data

---
+++ src/js/rank.js
@@ -0,0 +1,8 @@
+// localStorage에서 가져온 데이터
+const fetchedData = [
+  { username: 'Bareum', record: { mm: 0, ss: 0, ms: 10 } },
+  { username: 'Chaeyoung', record: { mm: 0, ss: 10, ms: 0 } },
+  { username: 'Sohyeong', record: { mm: 0, ss: 20, ms: 0 } },
+  { username: 'Hangyul', record: { mm: 0, ss: 30, ms: 0 } },
+  { username: 'Fastcampus', record: { mm: 0, ss: 40, ms: 0 } }
+];
