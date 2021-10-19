e0b826b7fc131b97441a4009586985c37aed6c9c
hanana1253 <hanana1253@gmail.com>
2021-10-19 18:12:37 +0900

    feat: Add formatRecord function
    TODO: fetching logic from window localStorage

--- src/js/rank.js
+++ src/js/rank.js
@@ -8,14 +8,28 @@ const fetchedData = [
 ];
 
 const render = () => {
+  // myName은 전역변수 상태를 가져올 예정이라 임시로 선언해둠
+  const myName = 'Hangyul';
+
   document.querySelector('.ranking-list').innerHTML =
     '<li class="header-row"><span>Rank</span> <span>Username</span><span>Record</span></li>' +
     fetchedData
-      .map((userData, index) => `<li><span>${index}</span><span>${userData.username}</span><span>${userData.record}</span></li>`)
+      .map((userData, index) => `<li ${userData.username === myName ? 'class="my-record"': ''}><span>${index}</span><span>${userData.username}</span><span>${formatRecord(userData.record)}</span></li>`)
       .join('');
-  document.querySelector('.my-rank').textContent = '';
-  document.querySelector('.my-result').textContent = '';
+
+  // my-rank와 my-result는 전역변수 상태를 가져와 넣을 예정
+  document.querySelector('.my-rank').textContent = '4';
+  document.querySelector('.my-result').textContent = '00:30:00';
+
   document.querySelector('.total-players').textContent = fetchedData.length;
 };
+
+// utils로 빼둘 함수
+const formatRecord = (() => {
+  // 1 => '01', 10 => '10'
+  const format = n => (n < 10 ? '0' + n : n + '');
+  return ({ mm, ss, ms }) => `${format(mm)}:${format(ss)}:${format(ms)}`;
+})();
+
 
 document.addEventListener('DOMContentLoaded', render);
