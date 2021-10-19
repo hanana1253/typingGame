600ab2af64ad8bb343abb9476fc0f2ff88f1ff4d
hanana1253 <hanana1253@gmail.com>
2021-10-19 17:54:27 +0900

    feat: JS script rendering logic with mock data

--- src/js/rank.js
+++ src/js/rank.js
@@ -6,3 +6,16 @@ const fetchedData = [
   { username: 'Hangyul', record: { mm: 0, ss: 30, ms: 0 } },
   { username: 'Fastcampus', record: { mm: 0, ss: 40, ms: 0 } }
 ];
+
+const render = () => {
+  document.querySelector('.ranking-list').innerHTML =
+    '<li class="header-row"><span>Rank</span> <span>Username</span><span>Record</span></li>' +
+    fetchedData
+      .map((userData, index) => `<li><span>${index}</span><span>${userData.username}</span><span>${userData.record}</span></li>`)
+      .join('');
+  document.querySelector('.my-rank').textContent = '';
+  document.querySelector('.my-result').textContent = '';
+  document.querySelector('.total-players').textContent = fetchedData.length;
+};
+
+document.addEventListener('DOMContentLoaded', render);
