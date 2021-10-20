f9517e66489890d5f5720a17da3a99878201fa08
hanana1253 <hanana1253@gmail.com>
2021-10-20 11:29:34 +0900

    feat: Rename render function to renderRanks

--- src/js/rank.js
+++ src/js/rank.js
@@ -15,7 +15,7 @@ const fetchedData = [
   { username: 'Fastcampus9', record: { mm: 3, ss: 40, ms: 0 } }
 ];
 
-const render = () => {
+const renderRanks = () => {
   // 아직 로컬스토리지에 저장하지 않아서 가져올 수가 없으므로 아래 주석처리
   // const fetchedData = window.localStorage('ranksList');
 
@@ -23,6 +23,9 @@ const render = () => {
   const myName = 'Chaeyoung';
   const myRank = 2;
   const myRecord = { mm: 1, ss: 30, ms: 0 };
+
+  // 전역상태변수에서 가져오는 함수
+
 
   document.querySelector('.ranks-ol').innerHTML = fetchedData
       .slice(0, 5)
@@ -60,4 +63,4 @@ const formatRecord = (() => {
   return ({ mm, ss, ms }) => `${format(mm)}:${format(ss)}:${format(ms)}`;
 })();
 
-document.addEventListener('DOMContentLoaded', render);
+document.addEventListener('DOMContentLoaded', renderRanks);
