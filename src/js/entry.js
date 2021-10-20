814cc0871e40f72cac362285249e59bc0ab37647
sosoYim <sohyung.yim@gmail.com>
2021-10-20 14:47:18 +0900

    fix: Change const to let about fetchedData

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,5 +1,5 @@
 // localStorage에서 가져온 데이터
-const fetchedData = [
+let fetchedData = [
   { username: 'Bareum', record: { mm: 0, ss: 0, ms: 10 } },
   { username: 'Chaeyoung', record: { mm: 0, ss: 10, ms: 0 } },
   { username: 'Sohyeong', record: { mm: 0, ss: 20, ms: 0 } },
