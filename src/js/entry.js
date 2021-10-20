ec5cf9f5c23e9971fc551946b596ab009159d85a
sosoYim <sohyung.yim@gmail.com>
2021-10-20 10:43:00 +0900

    feat: Add validate errors

--- src/js/entry.js
+++ src/js/entry.js
@@ -6,23 +6,20 @@ let rankingList = [
   { username: 'Fastcampus', record: { mm: 0, ss: 40, ms: 0 } }
 ];
 
-/*
-
-ranks 스토리지에 생성 (setRanks)
-ranks 의 name 중복확인 (isNameExist)
-ranks 에 새로운 기록 추가 (name 값 입력, record 초기값) (setRank)
-닉네임 정규표현식 (isValidName)
-*/
-
-const getRankingListLength = () => rankingList.length;
-
 // rankings 를 변수명으로 하는 것이 어떨까
-const setRankingList = ({ username, record = { mm: 0, ss: 0, ms: 0 } }) => {
+const setUserName = ({ username, record = { mm: 0, ss: 0, ms: 0 } }) => {
   rankingList = [...rankingList, { username, record }];
 };
 
 const isExistName = inputUserName =>
   rankingList.some(ranking => ranking.username === inputUserName);
+
+const isValidName = name => {
+  const regexp = new RegExp(/^[ㄱ-ㅎ|가-힣|a-z|A-Z|]{4,25}$/);
+  return regexp.test(name);
+};
+
+// EVENTS ====================================================================
 
 window.addEventListener('DOMContentLoaded', () => {
   if (!localStorage.getItem('rankingList'))
@@ -30,9 +27,21 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 document.getElementById('userName').oninput = e => {
+  const inputName = e.target.value;
+  const isValid = isExistName(inputName) && isValidName(inputName);
   const error = document.querySelector('.error');
-  // console.log(e);
-  error.textContent = isExistName(e.target.value)
+
+  error.textContent = isExistName(inputName)
     ? '이미 사용중인 이름입니다.'
+    : !isValidName(inputName)
+    ? '이름은 4~25자 사이의 문자로 사용할 수 있습니다.'
     : '';
+
+  document.querySelector('.start').setAttribute('disabled', isValid);
+};
+
+document.querySelector('.entry-form').onsubmit = () => {
+  // setting data
+  setUserName(document.querySelector('.entry-form #userName'));
+  // redirect to game start page
 };
