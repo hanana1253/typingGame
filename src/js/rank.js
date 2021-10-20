76a6de9ddce36a4197d276135ac8e4cdfe0761d6
hanana1253 <hanana1253@gmail.com>
2021-10-20 17:23:26 +0900

    feat: Add formatRecordFromMs function to get ms and format and applied in js file

--- src/js/rank.js
+++ src/js/rank.js
@@ -1,3 +1,5 @@
+import { getFromLocalStorage } from "./utils.js";
+
 // localStorage에서 가져온 데이터
 const fetchedData = [
   { username: 'Bareum', record: 10 },
@@ -17,40 +19,46 @@ const fetchedData = [
 
 const renderRanks = () => {
   // 아직 로컬스토리지에 저장하지 않아서 가져올 수가 없으므로 아래 주석처리
-  // const fetchedData = window.localStorage('ranksList');
+  // const fetchedData = getFromLocalStorage('records');
 
   // myName, myRank, myRecord는 전역변수 상태를 가져올 예정이라 임시로 선언해둠
-  const myName = 'Chaeyoung';
-  const myRank = 2;
-  const myRecord = 100000;
+  // const currentUser = getFromLocalStorage('currentUser');
+  const currentUser = { username: 'Chaeyoung', record: 110 }
+  const currentUserRank = fetchedData.findIndex(
+    userData => userData.username === currentUser.username
+  );
 
   // 전역상태변수에서 가져오는 함수
 
   document.querySelector('.ranks-ol').innerHTML = fetchedData
-      .slice(0, 5)
-      .map(
-        (userData, index) =>
-          `<li ${userData.username === myName ? 'class="my-record"' : ''}>
-            <span>${index+1}</span>
+    .slice(0, 5)
+    .map(
+      (userData, index) =>
+        `<li ${
+          userData.username === currentUser.username ? 'class="my-record"' : ''
+        }>
+            <span>${index + 1}</span>
             <span>${userData.username}</span>
             <span>${formatRecordFromMs(userData.record)}</span>
           </li>`
-      )
-      .join('');
+    )
+    .join('');
 
   // my-rank와 my-result는 전역변수 상태를 가져와 넣을 예정
-  document.querySelector('.my-rank').textContent = myRank;
-  document.querySelector('.my-result').textContent = formatRecordFromMs(myRecord);
+  document.querySelector('.my-rank').textContent = currentUserRank;
+  document.querySelector('.my-result').textContent = formatRecordFromMs(
+    currentUser.record
+  );
 
   document.querySelector('.total-players').textContent = fetchedData.length;
 
   // 5위권 밖인 경우 li 요소 추가 및 result 요소 높이변경(class추가하여)
-  if (myRank < 5) return;
+  if (currentUserRank < 5) return;
   const newListItem = document.createElement('li');
   newListItem.classList.add('added');
-  newListItem.innerHTML = `<span>${myRank}</span>
-      <span>${myName}</span>
-      <span>${formatRecordFromMs(myRecord)}</span>`;
+  newListItem.innerHTML = `<span>${currentUserRank}</span>
+      <span>${currentUser.username}</span>
+      <span>${formatRecordFromMs(currentUser.record)}</span>`;
   document.querySelector('.ranks-ol').appendChild(newListItem);
   document.querySelector('.result').classList.add('added');
 };
