4fe96f805383e7713baa3c20f3a917829ab19c1f
hanana1253 <hanana1253@gmail.com>
2021-10-21 13:44:23 +0900

    feat: Apply changed markup and class to make innerHTML

--- src/js/rank.js
+++ src/js/rank.js
@@ -1,28 +1,28 @@
 import { getFromLocalStorage, formatRecordFromMs } from './utils.js';
 
-// localStorage에서 가져온 데이터
-const fetchedData = [
-  { username: 'Bareum', record: 100 },
-  { username: 'Chaeyoung', record: 1100 },
-  { username: 'Sohyeong', record: 12490 },
-  { username: 'Fastcampus', record: 15000 },
-  { username: 'Fastcampus2', record: 60000 },
-  { username: 'Fastcampus3', record: 18000 },
-  { username: 'Fastcampus4', record: 20000 },
-  { username: 'Fastcampus5', record: 21000 },
-  { username: 'Fastcampus6', record: 23000 },
-  { username: 'Fastcampus7', record: 25000 },
-  { username: 'Hangyul', record: 29300 },
-  { username: 'Fastcampus8', record: 60000 },
-  { username: 'Fastcampus9', record: 129300 }
-];
+// // localStorage에서 가져온 데이터
+// const fetchedData = [
+//   { username: 'Bareum', record: 100 },
+//   { username: 'Chaeyoung', record: 1100 },
+//   { username: 'Sohyeong', record: 12490 },
+//   { username: 'Fastcampus', record: 15000 },
+//   { username: 'Fastcampus2', record: 60000 },
+//   { username: 'Fastcampus3', record: 18000 },
+//   { username: 'Fastcampus4', record: 20000 },
+//   { username: 'Fastcampus5', record: 21000 },
+//   { username: 'Fastcampus6', record: 23000 },
+//   { username: 'Fastcampus7', record: 25000 },
+//   { username: 'Hangyul', record: 29300 },
+//   { username: 'Fastcampus8', record: 60000 },
+//   { username: 'Fastcampus9', record: 129300 }
+// ];
 
-// // 실험용 localStorage 넣는 코드
-window.localStorage.setItem('records', JSON.stringify(fetchedData));
-window.localStorage.setItem(
-  'currentUser',
-  JSON.stringify({ username: 'Chaeyoung', record: 1100 })
-);
+// // // 실험용 localStorage 넣는 코드
+// window.localStorage.setItem('records', JSON.stringify(fetchedData));
+// window.localStorage.setItem(
+//   'currentUser',
+//   JSON.stringify({ username: 'Chaeyoung', record: 1100 })
+// );
 
 const renderRanks = () => {
   const fetchedData = getFromLocalStorage('records');
@@ -35,15 +35,15 @@ const renderRanks = () => {
   }
 
   // 5위까지 잘라서 보여주기.
-  document.querySelector('.ranks-ol').innerHTML = fetchedData
+  document.querySelector('.ranks-table-body').innerHTML = fetchedData
     .slice(0, 5)
     .map(
-      (userData, index) =>
-        `<li>
-      <span>${index + 1}</span>
-      <span class="username">${userData.username}</span>
-      <span>${formatRecordFromMs(userData.record)}</span>
-      </li>`
+      ({ username, record }, index) =>
+        `<tr>
+      <td>${index + 1}</td>
+      <td class="username">${username}</td>
+      <td>${formatRecordFromMs(record)}</td>
+      </tr>`
     )
     .join('');
 
@@ -60,19 +60,19 @@ const renderRanks = () => {
       userData => userData.username === currentUser.username
     ) + 1;
 
-  document.querySelector('.my-rank').textContent = currentUserRank;
-  document.querySelector('.my-result').textContent = formatRecordFromMs(
-    currentUser.record
-  );
-  document.querySelector('.total-players').textContent = fetchedData.length;
-  document.querySelector('.return').textContent = 'Try Again';
+  // document.querySelector('.my-rank').textContent = currentUserRank;
+  // document.querySelector('.my-result').textContent = formatRecordFromMs(
+  //   currentUser.record
+  // );
+  // document.querySelector('.total-players').textContent = fetchedData.length;
+  // document.querySelector('.return').textContent = 'Try Again';
 
   // currentUser가 5위 안에 드는 경우 my-record 클래스 이름 붙여주기
   if (currentUserRank < 5) {
-    [...document.querySelector('.ranks-ol').children].forEach($li => {
-      $li.classList.toggle(
+    [...document.querySelector('.ranks-table-body').children].forEach($tr => {
+      $tr.classList.toggle(
         'my-record',
-        currentUser.username === $li.querySelector('.username').textContent
+        currentUser.username === $tr.querySelector('.username').textContent
       );
     });
     return;
