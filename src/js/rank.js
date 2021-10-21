193fd1378dd2f9a9f1e0684086889b239baeefa9
hanana1253 <hanana1253@gmail.com>
2021-10-21 23:51:26 +0900

    refactor: Fix onclick event on page-control element
    - onclick의 state.currentPage에 모든 케이스를 다 넣어버림

--- src/js/rank.js
+++ src/js/rank.js
@@ -1,27 +1,25 @@
 import { PAGE_VIEW_LIMIT } from './constant.js';
-import {
-  getFromLocalStorage,
-  formatRecordFromMs
-} from './utils.js';
-
-const state = {
-  currentPage: 1,
-  lastPageNum: 0
-};
+import { getFromLocalStorage, formatRecordFromMs } from './utils.js';
 
 const currentUser = getFromLocalStorage('currentUser', '');
 const records = getFromLocalStorage('records', []);
+const lastPageNum = Math.ceil(records.length / PAGE_VIEW_LIMIT);
+const state = {
+  currentPage: 1
+};
 
 const render = () => {
-
-  // 데이터가 없는 경우, No records yet 메시지 노출 및 page-control hidden 후 return
   if (!records.length) {
-    document.querySelector('.ranks-table-head').textContent = 'No records yet';
-    document.querySelector('.page-control').classList.add('hidden');
+    document
+      .querySelector('.ranks-table')
+      .classList.toggle('hidden', !records.length);
+    document
+      .querySelector('.page-control')
+      .classList.toggle('hidden', !records.length);
+    document.querySelector('.result').textContent = 'NO RECORD YET';
+
     return;
   }
-  state.lastPageNum = Math.ceil(records.length / PAGE_VIEW_LIMIT);
-
 
   // 5개씩 잘라서 보여주기.
   document.querySelector('.ranks-table-body').innerHTML = (() => {
@@ -33,7 +31,7 @@ const render = () => {
     return currentPageRecords
       .map(
         ({ username, record }, index) =>
-          `<tr ${currentUser.username === username ? 'class="my-record"':''}>
+          `<tr ${currentUser.username === username ? 'class="my-record"' : ''}>
         <td>${(state.currentPage - 1) * PAGE_VIEW_LIMIT + index + 1}</td>
         <td>${username}</td>
         <td>${formatRecordFromMs(record)}</td>
@@ -42,56 +40,59 @@ const render = () => {
       .join('');
   })();
 
-  // currentPage 따라 처리한 ul 요소 동적 생성 및 추가
+  // currentPage 값에 따라 처리한 ul 요소 동적 생성 및 추가
   document.querySelector('.page-nums').innerHTML = (() => {
-    const indexOfFirst =
+    const pageStartNum =
       Math.floor((state.currentPage - 1) / PAGE_VIEW_LIMIT) * PAGE_VIEW_LIMIT +
       1;
-    const indexOfLast =
-      indexOfFirst + PAGE_VIEW_LIMIT - 1 < state.lastPageNum
-        ? indexOfFirst + PAGE_VIEW_LIMIT - 1
-        : state.lastPageNum;
+    const pageEndNum =
+      pageStartNum + PAGE_VIEW_LIMIT - 1 < lastPageNum
+        ? pageStartNum + PAGE_VIEW_LIMIT - 1
+        : lastPageNum;
 
     return Array.from(
-      { length: indexOfLast - indexOfFirst + 1 },
+      { length: pageEndNum - pageStartNum + 1 },
       (_, i) =>
         `<li ${
-          state.currentPage === indexOfFirst + i ? 'class="current"' : ''
-        }><a href="#">${indexOfFirst + i}</a></li>`
+          state.currentPage === pageStartNum + i ? 'class="current"' : ''
+        }><a href="#">${pageStartNum + i}</a></li>`
     ).join('');
   })();
 
+  if (!currentUser) return;
 
-  // currentUser 기록 보여주는 div에 넣을 것
   const currentUserRank =
     records.findIndex(({ username }) => username === currentUser.username) + 1;
 
+  document.querySelector('.result').innerHTML = ` 
+    <p>Your record : ${formatRecordFromMs(currentUser.record)}</p>
+    <p>You ranked ${currentUserRank} out of ${records.length} ${
+    records.length > 1 ? 'users' : 'user'
+  }!</p>`;
 };
 
-[...document.querySelectorAll('.prev-btn')].forEach($btn => {
-  $btn.onclick = () => {
-    if (state.currentPage === 1) return;
-    state.currentPage = $btn.classList.contains('to-first')
-      ? 1
-      : state.currentPage - 1;
-    render();
-  };
-});
+document.querySelector('.page-control').onclick = e => {
+  if (e.target === e.currentTarget || e.target.matches('.page-nums') ||
+    (e.target.matches('.prev-btn') && state.currentPage === 1) ||
+    (e.target.matches('.next-btn') && state.currentPage === lastPageNum))
+    return;
 
-[...document.querySelectorAll('.next-btn')].forEach($btn => {
-  $btn.onclick = () => {
-    if (state.currentPage === state.lastPageNum) return;
-    state.currentPage = $btn.classList.contains('to-last')
-      ? state.lastPageNum
-      : state.currentPage + 1;
-    render();
-  };
-});
+  state.currentPage = (() => {
+    if (e.target.matches('.prev-btn')) {
+      return e.target.classList.contains('to-first')
+        ? 1
+        : state.currentPage - 1;
+    }
+    if (e.target.matches('.next-btn'))
+      return e.target.classList.contains('to-last')
+        ? lastPageNum
+        : state.currentPage + 1;
+    if (e.target.matches('.page-nums a')) {
+      e.preventDefault();
+      return +e.target.textContent;
+    }
+  })();
 
-document.querySelector('.page-nums').onclick = e => {
-  if (!e.target.matches('.page-nums a')) return;
-  e.preventDefault();
-  state.currentPage = +e.target.textContent;
   render();
 };
 
