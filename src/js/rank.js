dd3187ff1cda2c7ba97c45db5eb3e9e015ed2be1
hanana1253 <hanana1253@gmail.com>
2021-10-22 10:24:56 +0900

    refactor: Change a tag to button

--- src/js/rank.js
+++ src/js/rank.js
@@ -6,18 +6,16 @@ const currentUser = getFromLocalStorage(STORAGE_KEY.CURRENT_USER, '');
 const records = getFromLocalStorage(STORAGE_KEY.RECORDS, []);
 const lastPageNum = Math.ceil(records.length / PAGE_VIEW_LIMIT);
 
-const state = {
-  currentPage: 1
-};
+let currentPage = 1;
 
 const render = () => {
-  if (!records.length) {
+  if (records.length === 0) {
     document
       .querySelector('.ranks-table')
-      .classList.toggle('hidden', !records.length);
+      .classList.add('hidden');
     document
       .querySelector('.page-control')
-      .classList.toggle('hidden', !records.length);
+      .classList.add('hidden');
     document.querySelector('.result').textContent = 'NO RECORD YET';
 
     return;
@@ -26,15 +24,15 @@ const render = () => {
   // 5개씩 잘라서 보여주기.
   document.querySelector('.ranks-table-body').innerHTML = (() => {
     const currentPageRecords = records.slice(
-      (state.currentPage - 1) * PAGE_VIEW_LIMIT,
-      state.currentPage * PAGE_VIEW_LIMIT
+      (currentPage - 1) * PAGE_VIEW_LIMIT,
+      currentPage * PAGE_VIEW_LIMIT
     );
 
     return currentPageRecords
       .map(
         ({ username, record }, index) =>
           `<tr ${currentUser.username === username ? 'class="my-record"' : ''}>
-        <td>${(state.currentPage - 1) * PAGE_VIEW_LIMIT + index + 1}</td>
+        <td>${(currentPage - 1) * PAGE_VIEW_LIMIT + index + 1}</td>
         <td>${username}</td>
         <td>${formatRecordFromMs(record)}</td>
       </tr>`
@@ -42,10 +40,9 @@ const render = () => {
       .join('');
   })();
 
-  // currentPage 값에 따라 처리한 ul 요소 동적 생성 및 추가
   document.querySelector('.page-nums').innerHTML = (() => {
     const pageStartNum =
-      Math.floor((state.currentPage - 1) / PAGE_VIEW_LIMIT) * PAGE_VIEW_LIMIT +
+      Math.floor((currentPage - 1) / PAGE_VIEW_LIMIT) * PAGE_VIEW_LIMIT +
       1;
     const pageEndNum =
       pageStartNum + PAGE_VIEW_LIMIT - 1 < lastPageNum
@@ -56,8 +53,8 @@ const render = () => {
       { length: pageEndNum - pageStartNum + 1 },
       (_, i) =>
         `<li ${
-          state.currentPage === pageStartNum + i ? 'class="current"' : ''
-        }><a href="#">${pageStartNum + i}</a></li>`
+          currentPage === pageStartNum + i ? 'class="current"' : ''
+        }><button type="button">${pageStartNum + i}</button></li>`
     ).join('');
   })();
 
@@ -74,23 +71,24 @@ const render = () => {
 };
 
 document.querySelector('.page-control').onclick = e => {
-  if (e.target === e.currentTarget || e.target.matches('.page-nums') ||
-    (e.target.matches('.prev-btn') && state.currentPage === 1) ||
-    (e.target.matches('.next-btn') && state.currentPage === lastPageNum))
-    return;
+  if (!e.target.matches('.page-control button') ||
+    (e.target.matches('.prev-btn') && currentPage === 1) ||
+    (e.target.matches('.next-btn') && currentPage === lastPageNum)){
+      console.log('invalid ')
+      return;
+    }
 
-  state.currentPage = (() => {
+  currentPage = (() => {
     if (e.target.matches('.prev-btn')) {
       return e.target.classList.contains('to-first')
         ? 1
-        : state.currentPage - 1;
+        : currentPage - 1;
     }
     if (e.target.matches('.next-btn'))
       return e.target.classList.contains('to-last')
         ? lastPageNum
-        : state.currentPage + 1;
-    if (e.target.matches('.page-nums a')) {
-      e.preventDefault();
+        : currentPage + 1;
+    if (e.target.matches('.page-nums button')) {
       return +e.target.textContent;
     }
   })();
